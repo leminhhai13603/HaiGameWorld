54cfcc33b5cf9abf9f1c9826393dfcd635de4af1
Minh Hai <leminhhai136a@gmail.com>
2026-06-10 13:57:40 +0700

    Remove Factory Empire game

--- js/hub.js
+++ js/hub.js
@@ -152,16 +152,6 @@ const Hub = {
             thumbClass: 'cyber-thumb',
             path: 'games/cyber-survivor/',
             featured: false
-        },
-        {
-            id: 'factory-empire',
-            name: 'Factory Empire',
-            desc: 'Xây dựng chuỗi sản xuất, tự động hoá tài nguyên, tối ưu logistics, đế chế công nghiệp!',
-            tag: 'Strategy',
-            thumb: '🏭',
-            thumbClass: 'factory-thumb',
-            path: 'games/factory-empire/',
-            featured: false
         }
     ],
 
